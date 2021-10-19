169be6ea3b37d7985be2af94bc984751d6dd117c
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-10-19 09:13:30 -0300

    made it work

--- Game_Loop/Program.cs
+++ Game_Loop/Program.cs
@@ -8,8 +8,70 @@ namespace Game_Loop
 {
 	class Program
 	{
+		static int X;
+		static int Y;
+		static bool gameOver;
+		static int userInput;
+
+		static void PlayerUpdate()
+		{
+			ConsoleKeyInfo Input = Console.ReadKey(true);
+
+			if (Input.Key == ConsoleKey.W)
+			{
+				Console.Clear();
+				Console.SetCursorPosition(X, Y--);
+				Console.WriteLine(".");
+				
+			}
+			if (Input.Key == ConsoleKey.S)
+			{
+				Console.Clear();
+				Console.SetCursorPosition(X, Y++);
+				Console.WriteLine(".");
+			}
+			if (Input.Key == ConsoleKey.A)
+			{
+				Console.Clear();
+				Console.SetCursorPosition(X--, Y);
+				Console.WriteLine(".");
+			}
+			if (Input.Key == ConsoleKey.D)
+			{
+				Console.Clear();
+				Console.SetCursorPosition(X++, Y); 
+				Console.WriteLine(".");
+			}
+		}
+
+
+		static void PlayerDraw()
+		{
+			Console.SetCursorPosition(X, Y);
+			Console.WriteLine(".");
+		}
+
 		static void Main(string[] args)
 		{
+			PlayerDraw();
+			while (gameOver == false)
+			{
+				PlayerUpdate();
+
+			}
+
+			if (gameOver == true)
+			{
+				Console.WriteLine("GAME OVER!!");
+
+			}
+
+
+
 		}
+
+
+
+
 	}
 }
